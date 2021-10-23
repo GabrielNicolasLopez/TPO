0168b469d34e015cf683825252df39187061c4a4
GabrielNicolasLopez <71357842+GabrielNicolasLopez@users.noreply.github.com>
2021-10-23 15:59:31 -0300

    Add files via upload

--- validaciones.js
+++ validaciones.js
@@ -8,48 +8,68 @@ const expresiones = {
     texto: /^[a-zA-Z0-9\_\-]+$/
 }
 
+const campos = {
+    nombre: false,
+    email: false,
+    texto: false
+}
+
 const validarDatos = (e) => {
     switch(e.target.name){
         case "nombre":
-            console.log("funciona");
             if(expresiones.nombre.test(e.target.value)){
                 document.getElementById('nombre').classList.remove('form-control-incorrect');
                 document.getElementById('nombre').classList.add('form-control');
+                campos.nombre = true;
             }else{
                 document.getElementById('nombre').classList.add('form-control-incorrect');
+                campos.nombre = false;
             }
         break;
 
         case "mail":
-            console.log("funciona");
             if(expresiones.mail.test(e.target.value)){
                 document.getElementById('mail').classList.remove('form-control-incorrect');
                 document.getElementById('mail').classList.add('form-control');
+                campos.email = true;
             }else{
                 document.getElementById('mail').classList.add('form-control-incorrect');
+                campos.email = false;
             }
         break;
     }
 }
 
-const validarTexto = (a) => {
-    console.log("funciono");
-    if(expresiones.texto.test(a.target.value)){
+const validarTexto = (e) => {
+    if(expresiones.texto.test(e.target.value)){
         document.getElementById('area-texto').classList.remove('form-control-incorrect');
         document.getElementById('area-texto').classList.add('form-control');
+        campos.texto = true;
     }else{
         document.getElementById('area-texto').classList.add('form-control-incorrect');
+        campos.texto = false;
     }
 }
 
 inputs.forEach((input) => {
     input.addEventListener('keyup', validarDatos);
+    input.addEventListener('keyup', cambiarNombre);
     input.addEventListener('blur', validarDatos);
 });
 
 textarea.forEach((textarea) => {
     textarea.addEventListener('keyup', validarTexto);
+    textarea.addEventListener('keyup', cambiarNombre);
     textarea.addEventListener('blur', validarTexto);
 });
+
+function cambiarNombre(){
+    var boton = document.getElementById('boton-enviar');
+    if(campos.nombre && campos.email && campos.texto){
+        boton.innerHTML = 'Enviar';
+    }else{
+        boton.innerHTML = 'Rellená el formulario';
+    }
+}
 
 
